50ca2771e10b9d83501c3dc0732d45a06105ae46
vgnvhfg <108231847+vgnvhfg@users.noreply.github.com>
2022-10-04 12:03:32 +0800

    week05

---
+++ week05-1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a=10;
+    //if(a==10)
+    //printf ("a¬O10\n");
+    while(a==10)
+        printf ("a¬O10\n");
+}
---
+++ week05-3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+
+    // int a=10;
+    //while(a>0)
+    //{
+    //printf("a¬O%i,a>0\n",a);
+    //a--;
+
+    for(int a=3; a>0; a--)
+    {
+        printf("a¬O%i,a>0\n",a);
+    }
+}
